59ec569bf74931d6429850dbbe4b1a161d5d4e88
Andres Manco <andresmanco@gmail.com>
2018-06-19 11:35:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,8 @@ def oxford_comma(array)
   if array.count ==2
     return array.join(" and ")
   elsif array.count > 2
-  n=1
-  array.each do |a|
-    if n == array.size
-      
-    return array.join(", ")
+    str = array.join(", ")
+    
     end
   end
   return array.join

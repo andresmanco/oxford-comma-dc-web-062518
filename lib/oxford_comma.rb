504bee9414d700ae3710b2b5cb7ae976aed96f66
Andres Manco <andresmanco@gmail.com>
2018-06-19 11:28:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.count ==2
     return array.join(" and ")
-  elsif array.count == 3
+  elsif array.count > 2
   return array.join(", ")
   end
   return array.join
